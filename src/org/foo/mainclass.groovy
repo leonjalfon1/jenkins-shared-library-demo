3a26ecb391169541b197d180e760992b8c6917cb
administrator <admin@admin.com>
2018-06-19 05:29:51 +0300

    update

--- src/org/foo/mainclass.groovy
+++ src/org/foo/mainclass.groovy
@@ -1,9 +1,10 @@
 package org.foo
-import static org.foo.helper.*
+//import static org.foo.helper.*
 
 class mainclass {
 static def mainfunction(script,prefix) {
-def msg = helperfunction()
+//def msg = helperfunction()
+def msg = "testmessage"
 script.sh """echo ${prefix}:${msg}"""
 }
 }
